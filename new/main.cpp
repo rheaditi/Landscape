0039ce83c6466205580733e8fb2d0b84dbfd6028
Aditi Anomita Mohanty <aditi.anomita@gmail.com>
2015-05-03 20:05:20 +0530

    + rotation + key inputs + polygons + grid

--- new/main.cpp
+++ new/main.cpp
@@ -4,7 +4,21 @@
 #include <sys/time.h>
 #include <math.h>
 
+#define SIZE 16
+
 using namespace std;
+
+struct index{
+    int i, j; };
+
+
+typedef struct {
+    float x, y, z;
+} point;
+
+int flagRegenerate=1;
+point mesh[SIZE+1][SIZE+1];
+double angle=0;
 
 void initialize();
 void display();
@@ -12,19 +26,9 @@ void myReshape(int w,int h);
 void keyboard(unsigned char key, int mousex, int mousey);
 void drawMesh();
 void generatePoints(float);
-
-struct index{
-    int i, j; };
-
+void setMaterial();
 void diamondSquare(index,index,index,index,int);
-
-typedef struct {
-    float x, y, z;
-} point;
-
-
-
-point mesh[31][31];
+void regenMesh(float);
 
 int main(int argc, char ** argv)
 {
@@ -45,19 +49,16 @@ int main(int argc, char ** argv)
 
 void display()
 {
-    int width=300;
-    index a, b, c, d;
+    int width=400;
 
     glClear(GL_DEPTH_BUFFER_BIT);
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
     gluLookAt(100,100,100,0.0,0.0,0.0,0.0,1.0,0.0);
-    //glRotated(45,0.0,1.0,0.0);
-    //glRotated(45,1.0,0.0,0.0);
-
-
 
     glColor4f(253/255.0, 227/255.0, 167/255.0,0.5);
+    //setMaterial();
+    glRotated(angle,0.0,1.0,0.0);
     glBegin(GL_LINE_LOOP);
         glVertex3f(-width,0.0,-width);
         glVertex3f(-width,0.0,width);
@@ -65,20 +66,8 @@ void display()
         glVertex3f(width,0.0,-width);
     glEnd();
 
-    glColor3f(239/255.0,72.0/255.0,54.0/255.0);
-    generatePoints(300.0);
-
-    a.i=0, a.j=0;
-    b.i=0, b.j=30;
-    c.i=30, c.j=30;
-    d.i=30, d.j=0;
-
-    mesh[a.i][a.j].y=40;
-    mesh[b.i][b.j].y=80;
-    mesh[c.i][c.j].y=160;
-    mesh[d.i][d.j].y=320;
-
-    diamondSquare(a,b,c,d,32);
+    if(flagRegenerate)
+        regenMesh(width);
     drawMesh();
 
     //glutWireCube(100.0);
@@ -87,10 +76,33 @@ void display()
 
 }
 
+void regenMesh(float width)
+{
+
+    index a, b, c, d;
+    generatePoints(width);
+
+    a.i=0, a.j=0;
+    b.i=0, b.j=SIZE;
+    c.i=SIZE, c.j=SIZE;
+    d.i=SIZE, d.j=0;
+
+    mesh[a.i][a.j].y=60;
+    mesh[b.i][b.j].y=40;
+    mesh[c.i][c.j].y=55;
+    mesh[d.i][d.j].y=40;
+
+    diamondSquare(a,b,c,d,SIZE);
+    flagRegenerate=0;
+}
+
 void initialize()
 {
     glEnable(GL_DEPTH_TEST);
-    //glShadeModel(GL_FLAT);
+    glShadeModel(GL_FLAT);
+    //glEnable(GL_LIGHTING);
+    //glEnable(GL_LIGHT0);
+    //glEnable(GL_NORMALIZE);
 }
 
 void myReshape(int w,int h)
@@ -105,23 +117,47 @@ void myReshape(int w,int h)
 
 void keyboard(unsigned char key, int mousex, int mousey)
 {
+    if(key=='D' || key=='d')
+        angle+=5;
+    else if(key=='A' || key =='a')
+        angle-=5;
+    else if(key=='q')
+        exit(0);
+    else if(key=='R' || key=='r')
+        flagRegenerate=1;
+    if(angle>360)
+        angle-=360;
    glutPostRedisplay();
 }
 
 void drawMesh()
 {
     int i=0, j=0;
-    for(i=0; i<30; i++)
+    for(i=0; i<SIZE; i++)
     {
-        for(j=0; j<30; j++)
+        for(j=0; j<SIZE; j++)
         {
-            glBegin(GL_LINE_LOOP);
+
+
+            glColor3f(239/255.0,72.0/255.0,54.0/255.0);
+            glBegin(GL_QUADS);
                 glVertex3f(mesh[i][j+1].x,mesh[i][j+1].y,mesh[i][j+1].z);
                 glVertex3f(mesh[i+1][j+1].x,mesh[i+1][j+1].y,mesh[i+1][j+1].z);
                 glVertex3f(mesh[i+1][j].x,mesh[i+1][j].y,mesh[i+1][j].z);
                 glVertex3f(mesh[i][j].x,mesh[i][j].y,mesh[i][j].z);
                  glFlush();
             glEnd();
+
+            glColor3f(0.0/255.0,0.0/255.0,0.0/255.0);
+            glBegin(GL_LINE_LOOP);
+                glVertex3f(mesh[i][j+1].x,mesh[i][j+1].y+1,mesh[i][j+1].z);
+                glVertex3f(mesh[i+1][j+1].x,mesh[i+1][j+1].y+1,mesh[i+1][j+1].z);
+                glVertex3f(mesh[i+1][j].x,mesh[i+1][j].y+1,mesh[i+1][j].z);
+                glVertex3f(mesh[i][j].x,mesh[i][j].y+1,mesh[i][j].z);
+                 glFlush();
+            glEnd();
+
+
         }
     }
 }
@@ -129,13 +165,13 @@ void drawMesh()
 void generatePoints(float width)
 {
     int i=0, j=0;
-    float zVal=-width, xVal=-width, incrementVal=(2.0*width)/30;
+    float zVal=-width, xVal=-width, incrementVal=(2.0*width)/SIZE;
     time_t t;
     int blah;
-    for(i=0; i<31; i++)
+    for(i=0; i<(SIZE+1); i++)
     {
         xVal=-width;
-        for(j=0; j<31; j++)
+        for(j=0; j<(SIZE+1); j++)
         {
             srand((unsigned)time(&t));
             mesh[i][j].x=xVal;
@@ -169,10 +205,10 @@ void diamondSquare(index a, index b, index c, index d, int iteration)
 
         srand(time(NULL));
 
-        mesh[abMid.i][abMid.j].y = (mesh[a.i][a.j].y+mesh[b.i][b.j].y)/2.0 - rand()%20;
-        mesh[bcMid.i][bcMid.j].y = (mesh[b.i][b.j].y+mesh[c.i][c.j].y)/2.0 - rand()%20;
-        mesh[cdMid.i][cdMid.j].y = (mesh[c.i][c.j].y+mesh[d.i][d.j].y)/2.0 - rand()%20;
-        mesh[daMid.i][daMid.j].y = (mesh[d.i][d.j].y+mesh[a.i][a.j].y)/2.0 - rand()%20;
+        mesh[abMid.i][abMid.j].y = (mesh[a.i][a.j].y+mesh[b.i][b.j].y)/2.0 + rand()%30;
+        mesh[bcMid.i][bcMid.j].y = (mesh[b.i][b.j].y+mesh[c.i][c.j].y)/2.0 + rand()%30;
+        mesh[cdMid.i][cdMid.j].y = (mesh[c.i][c.j].y+mesh[d.i][d.j].y)/2.0 + rand()%30;
+        mesh[daMid.i][daMid.j].y = (mesh[d.i][d.j].y+mesh[a.i][a.j].y)/2.0 + rand()%30;
 
         //Find The Center Of The Diamond
 
@@ -189,4 +225,23 @@ void diamondSquare(index a, index b, index c, index d, int iteration)
         diamondSquare(daMid,diamondCenter,cdMid,d,iteration/2);
 
     }
+}
+
+void setMaterial()
+{
+    float mat_ambient[] = {0.7,0.7,0.7,1.0};
+    float mat_diffuse[] = {0.5,0.0,0.0,1.0};
+    float mat_specular[] = {1.0,1.0,1.0};
+    float mat_shininess[] = {50.0};
+
+    glMaterialfv(GL_FRONT,GL_AMBIENT,mat_ambient);
+    glMaterialfv(GL_FRONT,GL_DIFFUSE,mat_diffuse);
+    glMaterialfv(GL_FRONT,GL_SPECULAR,mat_specular);
+    glMaterialfv(GL_FRONT,GL_SHININESS,mat_shininess);
+
+    float lightintensity[] = {1.0,1.0,1.0,1.0};
+    float light_position[] = {0.0,0,600.0,0.0};
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightintensity);
 }
